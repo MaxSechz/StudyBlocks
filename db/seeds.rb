390eae5cb17974bb9f90bff5806e0d371f6babab
Max Sechzer <msechzer@fandm.edu>
2015-01-09 09:26:14 -0500

    Add test objects to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+fandm = School.create(name: "Franklin & Marshall", country: "USA", state: "Pennsylvania", city: "Lancaster")
+me = fandm.users.create(username: "Maxwell", password: "maxwell", email: "msechzer@example.com")
+test_course = fandm.courses.create(title: "Intro to Testing Decks", course_code: "app110", instructor: "Test Guy")
+test_deck = me.decks.create(title: "Test", description: "This is the Test Deck", course_id: test_course.id)
+test_deck.cards.create([
+  {front: "Front side test", back: "Back side test", format: "response"},
+  {front: "Front side test", back: "Back side test", format: "response"}
+])
