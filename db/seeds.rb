c74907c43c868f439b8418ba0ed951ddf994e5a5
Max Sechzer <msechzer@fandm.edu>
2015-01-12 08:54:02 -0500

    Add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,5 +13,10 @@ test_deck = me.decks.create(title: "Test", description: "This is the Test Deck",
 me.registrations.create(course_id: test_course.id)
 test_deck.cards.create([
   {front: "Front side test", back: "Back side test", format: "response"},
-  {front: "Front side test2", back: "Back side test2", format: "response"}
+  {front: "Front side test2", back: "Back side test2", format: "response"},
+  {front: "Who was George Washington?", back: "The first president of the USA", format: "response"},
+  {front: "What is generally considered the smartest bird?", back: "The common crow", format: "response"},
+  {front: "Gary Numan is how many weeks older than Gary Oldman?", back: "Two weeks", format: "response"},
+  {front: "Who invented the word 'eyeball'?", back: "William Shakespeare", format: "response"},
+  {front: "What is Albus Dumbledore's not so secret vice?", back: "Candy", format: "response"}
 ])
