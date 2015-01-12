b3a73a8ee6ec555095de5b2d0a914cddd35de84c
Max Sechzer <msechzer@fandm.edu>
2015-01-12 00:44:45 -0500

    Begin dyanimc card form: response, choice, and boolean work

--- app/assets/javascripts/views/cards/card_form.js
+++ app/assets/javascripts/views/cards/card_form.js
@@ -3,25 +3,36 @@ StudyBlocks.Views.CardForm = Backbone.View.extend({
   className: "card-form",
   template: JST["cards/form"],
   events: {
-    "submit": "addCard"
+    "submit": "addCard",
+    "click input[type='radio']": "changeCardType"
+  },
+  subTemplates: {
+    response: JST["cards/text_form"],
+    choice: JST["cards/text_form"],
+    boolean: JST["cards/boolean_form"],
+    field: JST["cards/field_form"]
   },
 
   initialize: function (options) {
     this.submit = options.submit;
+    this.selected = this.model.escape('format');
   },
 
   render: function () {
-    var content = this.template({ card: this.model });
+    // this.setSelected();
+    var content = this.template({
+      card: this.model,
+      template: this.subTemplates[this.selected]
+    });
     this.$el.html(content);
-    this.setSelected();
     return this;
   },
 
   setSelected: function () {
-    if (this.model.get('format')) {
-      var jQuerySearch = "option[value=" + this.model.escape('format') + "]";
-      this.$el.find(jQuerySearch).prop({ selected: true });
-    }
+    // if (this.model.get('format')) {
+    //   var jQuerySearch = "input[value=" + this.model.escape('format') + "]";
+    //   this.$el.find(jQuerySearch).prop({ checked: true });
+    // }
   },
 
   addCard: function (event) {
@@ -41,5 +52,10 @@ StudyBlocks.Views.CardForm = Backbone.View.extend({
       thisForm.remove();
       thisDeck.cards().add(this.model, {merge: true});
     }
+  },
+
+  changeCardType: function (event) {
+    this.selected = $(event.currentTarget).val();
+    this.render();
   }
 });
